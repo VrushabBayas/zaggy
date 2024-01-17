0aac82436f80f1e704145bcd5f4b620ac55a9fc7
Vrushab Bayas <31801565+VrushabBayas@users.noreply.github.com>
2024-01-17 17:51:40 +0530

    display meal details from db show not found page on error

--- app/meals/[mealslug]/page.js
+++ app/meals/[mealslug]/page.js
@@ -1,5 +1,38 @@
 import React from "react";
 
-export default function MealDetails() {
-  return <h1>Meal Details</h1>;
+import classes from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
+import { notFound, useParams } from "next/navigation";
+
+export default function MealDetails({ params }) {
+  const slug = params.mealslug;
+  const meal = getMeal(slug);
+  if (!meal) {
+    notFound();
+  }
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill alt={meal.title} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}> {meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions.replace(/\n/g, "<br>"),
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
---
+++ app/meals/not-found.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Meal not found</h1>
+      <p>
+        Sorry, we couldn&apos;t find the meal you&apos;re looking for. Please
+        try again later.
+      </p>
+    </main>
+  );
+}
