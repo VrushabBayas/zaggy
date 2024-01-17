6bd9567a8cd3a0af501fe03dc995c46c5c42f537
Vrushab Bayas <31801565+VrushabBayas@users.noreply.github.com>
2024-01-17 15:43:36 +0530

    add meal page add meals and meal style

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,25 @@
 import React from "react";
+import Link from "next/link";
+
+import classes from "./page.module.css";
 
 export default function MealsPage() {
-  return <h1>MealsPage</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meals, created{" "}
+          <sapn className={classes.heighlight}> by you</sapn>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It&apos;s easy and
+          fun
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Sahre yout favorite Recipe</Link>
+        </p>
+      </header>
+      <main></main>
+    </>
+  );
 }
